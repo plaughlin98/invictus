a3c4be3ad2db54721664d0568e654ec1b6a19e95
plaughlin98 <plaughlin98@gmail.com>
2020-10-27 14:27:25 -0500

    Updates

---
+++ precache-manifest.64bbf9b8c3191b00bb6d3152e5ed931c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f060bfad2263ef638a7f24c1866d51fe",
+    "url": "/invictus/index.html"
+  },
+  {
+    "revision": "38ec955da9ec46b822dd",
+    "url": "/invictus/static/css/2.68584d97.chunk.css"
+  },
+  {
+    "revision": "9baebba22b7f2c99f472",
+    "url": "/invictus/static/css/main.71733af8.chunk.css"
+  },
+  {
+    "revision": "38ec955da9ec46b822dd",
+    "url": "/invictus/static/js/2.cf0b6282.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/invictus/static/js/2.cf0b6282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9baebba22b7f2c99f472",
+    "url": "/invictus/static/js/main.66d717ba.chunk.js"
+  },
+  {
+    "revision": "fbc245a09fc3b7b68f54",
+    "url": "/invictus/static/js/runtime-main.87f758e4.js"
+  },
+  {
+    "revision": "617369a289cf81952c0e935f7fe2ce3d",
+    "url": "/invictus/static/media/happydoctor2.617369a2.jpg"
+  }
+]);
